e7f39dabf000f61cdc169448c49b099d46d2f845
kmats <matsumoto_k@aid-dcc.com>
2017-07-25 18:04:41 +0900

    FirstCommit

---
+++ ShaderTest/Assets/Editor/ChangeShaderInspector.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ChangeShaderInspector : MaterialEditor
+{
+    bool isSquare = true;
+    float isSquareF = 1.0f;
+	// マテリアルへのアクセス
+	Material material
+	{
+		get
+		{
+			return (Material)target;
+		}
+	}
+
+	// Inspectorに表示される内容
+	public override void OnInspectorGUI()
+	{
+		// マテリアルを閉じた時に非表示にする
+		if (isVisible == false) { return; }
+
+		// 入力内容が変更されたかチェック
+		EditorGUI.BeginChangeCheck();
+
+		// InspectorのGUIを定義
+		//Texture mainTex = EditorGUILayout.ObjectField(
+		//"main texture",
+		//material.GetTexture("_MainTex"),
+		//typeof(Texture),
+		//false) as Texture;
+		Color color1 = EditorGUILayout.ColorField(
+			"Color1",
+			material.GetColor("_Color1")); 
+        Color color2 = EditorGUILayout.ColorField(
+	        "Color2",
+	        material.GetColor("_Color2"));
+
+        isSquare = EditorGUILayout.Toggle("isSquare", isSquare);
+        int boxes=0, boxesH=0, boxesW=0;
+        if(isSquare){
+            isSquareF = 1.0f;
+            boxes = EditorGUILayout.IntField("Boxes",
+                                                 material.GetInt("_Boxes"));
+        } else {
+			isSquareF = 0.0f;
+			boxesH = EditorGUILayout.IntField("BoxH",
+												 material.GetInt("_BoxesH"));
+            boxesW = EditorGUILayout.IntField("BoxW",
+                                              material.GetInt("_BoxesW"));
+		}
+
+		// 更新されたら内容を反映
+		if (EditorGUI.EndChangeCheck())
+		{
+			//material.SetTexture("_MainTex", mainTex);
+			material.SetColor("_Color1", color1);
+			material.SetColor("_Color2", color2);
+            material.SetFloat("_isSquare",isSquareF);
+            if (isSquare)
+            {
+                material.SetInt("_Boxes", boxes);
+            } else
+            {
+                material.SetInt("_BoxesH", boxesH);
+                material.SetInt("_BoxesW", boxesW);
+            }
+		}
+	}
+}
---
+++ ShaderTest/Assets/Ichimatsu.shader
@@ -0,0 +1,79 @@
+﻿Shader "Unlit/Ichimatsu"
+{
+	Properties
+	{
+		//_MainTex ("Texture", 2D) = "white" {}
+        _Color1 ("Color1", Color) = (1,1,1,1)
+        _Color2 ("Color2", Color) = (0,0,0,0)
+        _Boxes ("Boxes", int) = 2
+        _BoxesH ("BoxH", int) = 2
+        _BoxesW ("BoxW", int) = 2
+        [MaterialToggle] _isSquare ("isSquare", Float) = 0 
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			//sampler2D _MainTex;
+			//float4 _MainTex_ST;
+            fixed4 _Color1;
+            fixed4 _Color2;
+            int _Boxes;
+            int _BoxesH;
+            int _BoxesW;
+            Float _isSquare;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                if(_isSquare == 1.0) {
+                    o.uv = v.uv * _Boxes;
+                } else {
+	                o.uv.x = v.uv.x * _BoxesW;
+	                o.uv.y = v.uv.y * _BoxesH;
+                }
+                
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+                float2 c = i.uv;    // uv座標
+                c = floor(c) / 2;   // 2でわる（1.1->0.55）（0.7->0.35)
+                float checker = frac(c.x + c.y) * 2;
+                // 足して小数点以下入れる
+                // 0.35+0.35=0.7
+                if(checker==1) return _Color1;
+                return _Color2;
+		
+			}
+			ENDCG
+		}
+	}
+    CustomEditor "ChangeShaderInspector"
+}
